ab499c623323efc7964351eece83c0f4c78f57a3
faryal <faryal.atique1991@gmail.com>
2024-12-18 00:53:37 +0500

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -10,18 +10,20 @@ const Hero = () => {
         <main className="flex items-center w-screen h-[900px] bg-[#FBEBB5]">
 
             {/* Main Heading */}
-
-            <div className="flex flex-col justify-between items-start ml-[202px] w-[440px] h-[276px]">
+        <section className="flex">
+            <div>
+            <div className="flex flex-col justify-center items-center h-[900px]">
                 <h2 className={`${poppins.className} w-full h-48 text-[64px] leading-[96px]`}>
                     Rocket single seater</h2>
                 <button className={`${poppins.className} w-[121px] h-[49px] text-[20px] leading-[36px] border-b-2 border-black`}>
                     Shop Now
                 </button>
             </div>
+            </div>
             <Image src={"/Rocket.png"} alt={"sofa"} width={803} height={1068}/>
+        </section>
 
-        </main>
-    )
-}
+            </main>    
+    )};
 
 export default Hero
